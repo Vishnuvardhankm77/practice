9e388687943888462956628287f4c40254180e83
Vishnuvardhankm77 <vishnuvardhankm77@gmail.com>
2023-11-14 17:18:08 +0530

    image slider

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Image from './Data';
+
+function App() {
+  return (
+    <div>
+        <Image></Image>
+    </div>
+  );
+}
+
+export default App;
