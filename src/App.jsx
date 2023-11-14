69a8393fd1abaff7b8bb2270b6fc06c405c76c7c
Vishnuvardhankm77 <vishnuvardhankm77@gmail.com>
2023-11-14 11:49:41 +0530

    Counter

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+       <center>
+          <Counter></Counter>
+       </center>
+    </div>
+  );
+}
+
+export default App;
